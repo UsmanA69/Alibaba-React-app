34abd50dcd33012661fb3a6c273aaf84a24bd487
unknown <usmanarain7755@gmail.com>
2021-11-02 22:37:12 +0500

    my alibaba web

---
+++ src/myMarket.js
@@ -0,0 +1,35 @@
+const Market = [
+  {
+    image: "images/categories/electronics.png",
+    text: "Consumer Electronics",
+  },
+  {
+    image: "images/categories/apparel.png",
+    text: "Apparel",
+  },
+  {
+    image: "images/categories/vehicles.jpeg",
+    text: "Vehicles & Accessories",
+  },
+  {
+    image: "images/categories/sports.jpeg",
+    text: "Sports & Entertainment",
+  },
+  {
+    image: "images/categories/machinery.png",
+    text: "Machinery",
+  },
+  {
+    image: "images/categories/home.png",
+    text: "Home & Garden",
+  },
+  {
+    image: "images/categories/beauty.png",
+    text: "Beauty & Personal Care",
+  },
+  {
+    image: "images/categories/all-category.png",
+    text: "All Categories",
+  },
+];
+export default Market;
